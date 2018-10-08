5175a96b60ef7a84c8c98640cdb27a20fec7f5dd
Josh Cunningham <josh@joshcanhelp.com>
2018-10-08 13:27:44 -0700

    Add new login_ro method to replace login

--- spec/integration/lib/auth0/api/api_authentication_spec.rb
+++ spec/integration/lib/auth0/api/api_authentication_spec.rb
@@ -1,16 +1,17 @@
 require 'spec_helper'
 describe Auth0::Api::AuthenticationEndpoints do
-  attr_reader :client, :test_user_email, :test_user
+  attr_reader :client, :test_user_email, :test_user_pwd, :test_user
 
   before(:all) do
     @client = Auth0Client.new(v2_creds)
 
-    @test_user_email = "#{entity_suffix}-username-1}@auth0.com"
+    @test_user_email = "#{entity_suffix}-username-1@auth0.com"
+    @test_user_pwd = '23kejn2jk3en2jke2jk3be2jk3ber'
 
     VCR.use_cassette('Auth0_Api_AuthenticationEndpoints/create_test_user') do
       @test_user ||= @client.signup(
         test_user_email,
-        Faker::Internet.password
+        test_user_pwd
       )
     end
   end
@@ -48,6 +49,57 @@ describe Auth0::Api::AuthenticationEndpoints do
   describe '.wsfed_metadata', vcr: true do
     it 'should retrieve WSFED metadata' do
       expect(@client.wsfed_metadata).to(include('<EntityDescriptor'))
+    end
+  end
+
+  describe '.login_ro', vcr: true do
+    it 'should fail with an incorrect email' do
+      expect do
+        @client.login_ro(
+          test_user['email'] + '_invalid',
+          test_user_pwd
+        )
+      end.to raise_error Auth0::AccessDenied
+    end
+
+    it 'should fail with an incorrect password' do
+      expect do
+        @client.login_ro(
+          test_user['email'],
+          test_user_pwd + '_invalid'
+        )
+      end.to raise_error Auth0::AccessDenied
+    end
+
+    it 'should login successfully with a default scope' do
+      expect(
+        @client.login_ro(
+          test_user['email'],
+          test_user_pwd
+        )
+      ).to include( 'access_token', 'id_token', 'expires_in', 'scope' )
+    end
+
+    it 'should fail with an invalid audience' do
+      expect do
+        @client.login_ro(
+          test_user['email'],
+          test_user_pwd,
+          scope: 'test:scope',
+          audience: 'https://brucke.club/invalid/api/v1/'
+        )
+      end.to raise_error Auth0::BadRequest
+    end
+
+    it 'should login successfully with a custom audience' do
+      expect(
+        @client.login_ro(
+          test_user['email'],
+          test_user_pwd,
+          scope: 'test:scope',
+          audience: 'https://brucke.club/custom/api/v1/'
+        )
+      ).to include( 'access_token', 'expires_in' )
     end
   end
 end
